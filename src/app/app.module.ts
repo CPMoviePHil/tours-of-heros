378af95e844736a426e0f8e95c19b5e5ed491a20
cpmoviephil <you9215712@gmail.com>
2020-05-30 15:15:29 +0800

    hero try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { HerosComponent } from './heros/heros.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HerosComponent implements OnInit {
 
+
+  heros;
+  hero: Hero = {
+    id:1,
+    name:'Windstorms',
+  }
   constructor() { }
 
   ngOnInit(): void {
+    //this.hero = 'GigiTsaDa';
+    this.heros = [
+      {
+        'name':'jordon',
+        'age': 21,
+        'ranking': 1,
+      },
+      {
+        'name':'jackson',
+        'age': 42,
+        'ranking': 2,
+      },
+      {
+        'name':'Phil',
+        'age': 24,
+        'ranking': 3,
+      },
+
+    ];
   }
 
 }
