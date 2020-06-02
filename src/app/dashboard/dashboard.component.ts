a2620785a60053a0ef81c20760d352e5991edadb
cpmoviephil <you9215712@gmail.com>
2020-06-02 21:25:43 +0800

    route + dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeros();
+  }
+
+
+  getHeros(): void{
+    this.heroService.getHeroes().subscribe(hero_i => this.heroes = hero_i.slice(1,5));
   }
 
 }
